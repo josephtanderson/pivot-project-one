c88800fa1e4c51944ac17a9116b9a99c301545d7
Joseph Anderson <josephtanderson91@gmail.com>
2022-03-22 22:31:04 -0500

    calculating working - no styling

--- js/calculator-project.js
+++ js/calculator-project.js
@@ -1,29 +1,12 @@
-/*
-const allClear = document.getElementById("all-clear");
-const del = document.getElementById("delete");
-const divide = document.getElementById("divide");
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const multiply = document.getElementById("multiply");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const add = document.getElementById("add");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const subtract = document.getElementById("subtract");
-const decimal = document.getElementById("decimal");
-const zero = document.getElementById("zero");
+const allClear = document.getElementById("clear");
+const numbers = document.querySelectorAll("[number-btn]");
+const operators = document.querySelectorAll("[operator-btn]");
+const del = document.querySelector("[del-btn]");
 const equals = document.getElementById("equals");
-*/
 const prev = document.getElementById("previous");
 const curr = document.getElementById("current");
 
-
 class Calculator{
-    operation = "";
     operator = "";
     previousOperand = "";
     currentOperand = "";
@@ -42,67 +25,84 @@ class Calculator{
             this.currentOperandTextEle = "";
         }
     }
-
     delete(){
-        this.currentOperandTextEle = this.currentOperandTextEle.slice(0, this.currentOperandTextEle.length-1)
+        this.currentOperandTextEle = this.currentOperandTextEle.slice(0, -1);
     }
     appendNumber(num){
+        if (!this.operator && this.currentOperandTextEle === "") {
+            this.clear();
+        }
         if (num === "." && this.currentOperandTextEle.includes(".")) {
         } else {
             this.currentOperandTextEle += num;
         }
     }
     chooseOperation(operator){
-            this.previousOperand = Number(this.currentOperandTextEle);
-            console.log(this.previousOperand, "Line 59")
-            this.currentOperandTextEle = "";
-            this.operator = operator;
+        if (this.operator) {this.compute()};
+        if (this.currentOperandTextEle) {
+                this.previousOperand = Number(this.currentOperandTextEle);
+                this.currentOperandTextEle = "";
+            }
+        this.operator = operator;
     }
     compute(){
         this.currentOperand = Number(this.currentOperandTextEle);
         switch (this.operator) {
-            case "add":
-                this.previousOperandTextEle = this.previousOperand + this.currentOperand;
+            case "+":
+                this.previousOperand = this.previousOperand + this.currentOperand;
                 break;
-            case "subtract":
-                this.previousOperandTextEle = this.previousOperand - this.currentOperand;
+            case "-":
+                this.previousOperand = this.previousOperand - this.currentOperand;
                 break;
-            case "multiply":
-                this.previousOperandTextEle = (this.previousOperand * this.currentOperand);
-                this.operation = `${this.previousOperand} x ${this.currentOperand} =`
-                this.currentOperandTextEle = "";
+            case "x":
+                this.previousOperand = this.previousOperand * this.currentOperand;
                 break;
-            case "divide":
-                this.previousOperandTextEle = this.previousOperand / this.currentOperand;
+            case "÷":
+                this.previousOperand = this.previousOperand / this.currentOperand;
                 break;
+            default:
+                this.previousOperand = this.currentOperand;
         }
-        
+        this.previousOperandTextEle = this.previousOperand.toString();
+        this.currentOperandTextEle = "";
+        this.currentOperand = "";
+        this.operator = "";
     }
 }
-
 
 const calcExample = new Calculator();
 
-calcExample.appendNumber("4");
-calcExample.appendNumber(".");
-calcExample.appendNumber("2");
-calcExample.appendNumber(".");
-calcExample.appendNumber("2");
-calcExample.chooseOperation("multiply");
-calcExample.appendNumber("1");
-calcExample.appendNumber("9");
-calcExample.compute();
+function updateDisplay() {
+    prev.textContent = calcExample.previousOperandTextEle.toString()
+    curr.textContent = calcExample.previousOperand.toString() +" "+calcExample.operator+" "+calcExample.currentOperandTextEle.toString();
+};
+updateDisplay();
 
-console.log(calcExample);
+for (let i=0;i<numbers.length; i++) {
+    numbers.item(i).addEventListener('click', (event) => {
+        calcExample.appendNumber(event.target.innerText);
+        updateDisplay();
+    });
+};
+for (let i=0; i<operators.length; i++) {
+    operators.item(i).addEventListener('click', (event) => {
+        calcExample.chooseOperation(event.target.innerText);
+        updateDisplay();
+    });
+};
+allClear.addEventListener('click', (event) => {
+    calcExample.clear();
+    updateDisplay();
+});
+del.addEventListener('click', (event) => {
+    calcExample.delete();
+    updateDisplay();
+});
+equals.addEventListener('click', (event) => {
+    calcExample.compute();
+    updateDisplay();
+});
 
-prev.textContent = calcExample.previousOperandTextEle.toString();
-curr.textContent = calcExample.currentOperandTextEle.toString();
-if (!calcExample.currentOperandTextEle) {
-    prev.textContent = calcExample.operation;
-    curr.textContent = calcExample.previousOperandTextEle.toString();
-    if (!calcExample.previousOperandTextEle) {
-        prev.textContent = "";
-        curr.textContent = "0";
-    }
-}
-
+curr.addEventListener('click', (event) => {
+    console.log(calcExample);
+})
